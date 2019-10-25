03b0f520999e8b02327f63ae90507b99bd4ecfd9
Budiharta <budiharta_21@live.com>
2019-10-25 12:01:17 +0800

    set state and styling the element

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ class App extends React.Component {
     this.state = { is_dark: false }; //it called state
   }
 
+  toogleMode = () => {
+    this.setState({ is_dark: !this.state.is_dark });
+  }
+
   render() {
     const { is_dark } = this.state; //you can call the state like this
 
-    return (<div>
+    // dont forget to create the style
+    const containerStyle = { 
+      display: "flex", 
+      height: "100vh", 
+      alignItems: "center",
+      justifyContent: "center", 
+      flexDirection: "column",
+      background: is_dark ? "black" : "white",
+      color: is_dark ? "white" : "black",
+    };
+
+    const buttonStyle = {
+      padding: "8px",
+      background: "white",
+      borderRadius: "8px",
+    }
+
+    return (<div style={containerStyle}>
       <h2>Hello world!</h2>
-      <button>Turn {is_dark ? "on" : "off"} the light</button>
+      <button style={buttonStyle} onClick={this.toogleMode}>Turn {is_dark ? "on" : "off"} the light</button>
     </div>)
   }
 }
