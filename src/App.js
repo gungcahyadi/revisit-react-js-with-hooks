19ee23a2041d90f94591c48d58b923615a20dec4
Budiharta <budiharta_21@live.com>
2019-10-25 12:18:26 +0800

    make it use hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { is_dark: false }; //it called state
+const App = () => {
+  const [ is_dark, setIsDark ] = useState(false);
+
+  const containerStyle = { 
+    display: "flex", 
+    height: "100vh", 
+    alignItems: "center",
+    justifyContent: "center", 
+    flexDirection: "column",
+    background: is_dark ? "black" : "white",
+    color: is_dark ? "white" : "black",
+  };
+
+  const buttonStyle = {
+    padding: "8px",
+    background: "white",
+    borderRadius: "8px",
   }
 
-  toogleMode = () => {
-    this.setState({ is_dark: !this.state.is_dark });
+  function toogleMode() {
+    setIsDark(!is_dark);
   }
 
-  render() {
-    const { is_dark } = this.state; //you can call the state like this
-
-    // dont forget to create the style
-    const containerStyle = { 
-      display: "flex", 
-      height: "100vh", 
-      alignItems: "center",
-      justifyContent: "center", 
-      flexDirection: "column",
-      background: is_dark ? "black" : "white",
-      color: is_dark ? "white" : "black",
-    };
-
-    const buttonStyle = {
-      padding: "8px",
-      background: "white",
-      borderRadius: "8px",
-    }
-
-    return (<div style={containerStyle}>
-      <h2>Hello world!</h2>
-      <button style={buttonStyle} onClick={this.toogleMode}>Turn {is_dark ? "on" : "off"} the light</button>
-    </div>)
-  }
+  return (<div style={containerStyle}>
+    <h2>Hello world!</h2>
+    <button style={buttonStyle} onClick={toogleMode}>Turn {is_dark ? "on" : "off"} the light</button>
+  </div>)
 }
 
 export default App;
