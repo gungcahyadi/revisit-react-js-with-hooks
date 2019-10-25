78aeaefa9fa473729a7a27991b3fa0455e621482
Budiharta <budiharta_21@live.com>
2019-10-25 11:53:28 +0800

    create component with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { is_dark: false }; //it called state
+  }
+
   render() {
+    const { is_dark } = this.state; //you can call the state like this
+
     return (<div>
       <h2>Hello world!</h2>
-    </div>);
+      <button>Turn {is_dark ? "on" : "off"} the light</button>
+    </div>)
   }
 }
 
